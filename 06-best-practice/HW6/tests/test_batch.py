85f83e121e85fa90251ee5a1896fd2be215256c2
tz <157868810+tzhsu211@users.noreply.github.com>
2025-07-13 21:20:07 +0000

    HW6

---
+++ 06-best-practice/HW6/tests/test_batch.py
@@ -0,0 +1,43 @@
+
+from datetime import datetime
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+import batch
+
+
+
+def dt(hour, minute, second =0):
+    return datetime(2023,1,1, hour, minute, second)
+
+
+
+def test_prepare_data():
+    
+    categorical = ['PULocationID', 'DOLocationID']
+    
+    data = [
+    (None, None, dt(1, 1), dt(1, 10)),
+    (1, 1, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+    ]
+
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    df = pd.DataFrame(data, columns=columns)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    expected_dataframe = [
+        ('-1', '-1', dt(1, 1), dt(1, 10), 9.0),
+        ('1', '1', dt(1, 2), dt(1, 10), 8.0),
+        ]
+    
+    expected_dataframe = pd.DataFrame(expected_dataframe, columns = columns+['duration'])
+    
+    assert_frame_equal(df, expected_dataframe)
